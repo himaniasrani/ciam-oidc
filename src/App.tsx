5b05a11bb20907227581362bb992136b092147f5
Himani <himani.asrani@gslab.com>
2022-04-21 19:14:44 +0530

    Change onelogin instance

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ import jwt_decode from "jwt-decode";
 const codeVerifier =
     "hkoO3~j4~18XUA3iJkkJqdI8qfP98.sc8hKL48W6FlxlihlpLB1MbfLjMlLm7fnY9voa8_op8EpWCgdbIwfPWDzuSLuaDDa68SpkZ_7m40eDJeCAKMWqsrFYmSbxNfgi";
 const codeChallenge = "_k1W_xDVp_9zLHF90NLkZDbE41R4LhpMRrhRU0rQNhQ";
-const client_id = "9bf2aac0-5427-013a-59a3-0263b45f15b9202740";
+const client_id = "436c0950-a373-013a-7061-0234d2b88adb155934";
 const SCOPE = "openid profile email";
 const STATE = "1211212";
 const CALLBACK_URL = "https://ciam-oidc.pages.dev";
@@ -18,7 +18,7 @@ const CALLBACK_URL = "https://ciam-oidc.pages.dev";
 function App() {
     const [userData, setUserData] = useState<any>();
     const [userInfo, setUserInfo] = useState<any>();
-    const authURL = `https://himani-gslab-dev.onelogin.com/oidc/2/auth?response_type=code&code_challenge=${codeChallenge}&code_challenge_method=S256&client_id=${client_id}&redirect_uri=${CALLBACK_URL}&scope=${SCOPE}&state=${STATE}`;
+    const authURL = `https://gshrishi-dev.onelogin.com/oidc/2/auth?response_type=code&code_challenge=${codeChallenge}&code_challenge_method=S256&client_id=${client_id}&redirect_uri=${CALLBACK_URL}&scope=${SCOPE}&state=${STATE}`;
 
     useEffect(() => {
         const code = new URLSearchParams(
@@ -33,7 +33,7 @@ function App() {
                 redirect_uri: CALLBACK_URL,
               } );
             axios
-                .post("https://himani-gslab-dev.onelogin.com/oidc/2/token", params, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
+                .post("https://gshrishi-dev.onelogin.com/oidc/2/token", params, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
                 .then((data) => {
                     setUserData(data);
                     setUserInfo(jwt_decode(data['data']['id_token']));
@@ -53,7 +53,7 @@ function App() {
             {userData && userInfo && (
                 <div>
                     <h2>Hello {userInfo['name']}!</h2>
-                    {JSON.stringify(userData)}
+                    <h3>Email : {userInfo['email']}</h3>
                 </div>
             )}
         </div>
